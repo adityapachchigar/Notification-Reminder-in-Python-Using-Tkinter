28c05596d8972668771354e24b5dd588ee3ec72b
adityapachchigar <86509170+adityapachchigar@users.noreply.github.com>
2022-08-09 10:31:41 +0530

    Add files via upload

---
+++ PSC.py
@@ -0,0 +1,188 @@
+import tkinter as tk
+from tkinter import simpledialog
+from plyer import notification
+import time
+
+root = tk.Tk()
+root.title("Notification Reminder")
+flag=0
+
+ttl = ""
+msg = ""
+
+def setReminder(title, message):
+    ttl = title
+    msg = message
+    notification.notify(title = ttl,
+        message = msg,
+        app_name = "Reminder",
+        app_icon = 'C:/Users/admin/Documents/VS Codes/Innovative/icon.ico',
+        timeout = 10,
+        ticker = "oop")
+    # time.sleep(86400)
+
+def clock():
+    hour=time.strftime("%H")
+    minute=time.strftime("%M")
+    second=time.strftime("%S")
+    my_label.config(text=hour+":"+minute+":"+second)
+    my_label.after(1000,clock)
+
+def reminder():
+    f1=open("C:/Users/admin/Documents/VS Codes/Innovative/"+"data.txt","a+")
+    title = simpledialog.askstring("Make Reminder", "Enter Reminder Title")
+    message = simpledialog.askstring("Make Reminder", "Enter Reminder Description")
+    global tm
+    tm = simpledialog.askfloat("Make Reminder", "Enter minutes after which you want to receive notification")
+    global visual
+    s1=title+","+message+","+str(tm)+"\n"
+    f1.write(s1)
+    tms=int(tm*60)
+
+    visual = tk.Label(frame, text = title+"\t\t\t"+message+"\t\t\t"+str(tm)+" minutes", bg = "white")
+    visual.pack()
+    root.after(1000 * tms, setReminder, title, message)
+    
+def deleteReminder():
+    root.after(1000,visual.destroy)
+
+board = tk.Canvas(root, width=800, height=550, bg="green")
+board.pack()
+
+frame = tk.Frame(root, bg="white")
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+label1=tk.Label(frame,text="CURRENT TIME", bg="white")
+label1.pack()
+my_label=tk.Label(frame, text="",font=("Helvetica",40),fg="green")
+my_label.pack()
+clock()
+
+head=tk.Label(frame,text="TITLE\t\t\t\tMESSAGE\t\t\t\tTIME")
+head.pack()
+
+newReminder = tk.Button(root, text="Add Reminder", padx=10, pady=5,fg="black", bg="white",activebackground="green", command = reminder)
+deleteReminder=tk.Button(root, text="Delete Reminder", padx=10, pady=5,fg="black", bg="white",activebackground="red", command = deleteReminder)
+newReminder.pack()
+deleteReminder.pack()
+
+root.mainloop()
+
+# from django.shortcuts import render, redirect
+# from django.http import HttpResponse
+# from django.contrib.auth.models import User
+# from django.contrib import messages
+# from django.core.mail import EmailMessage, send_mail
+# from django.contrib.sites.shortcuts import get_current_site
+# from django.template.loader import render_to_string
+# from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+# from django.utils.encoding import force_bytes, force_text
+# from django.contrib.auth import authenticate, login, logout
+
+# # Create your views here.
+# def home(request):
+#     return render(request, "authentication/index.html")
+
+# def signup(request):
+#     if request.method == "POST":
+#         username = request.POST['username']
+#         fname = request.POST['fname']
+#         lname = request.POST['lname']
+#         email = request.POST['email']
+#         pass1 = request.POST['pass1']
+#         pass2 = request.POST['pass2']
+        
+#         if User.objects.filter(username=username):
+#             messages.error(request, "Username already exist! Please try some other username.")
+#             return redirect('home')
+        
+#         if User.objects.filter(email=email).exists():
+#             messages.error(request, "Email Already Registered!!")
+#             return redirect('home')
+        
+#         if len(username)>20:
+#             messages.error(request, "Username must be under 20 charcters!!")
+#             return redirect('home')
+        
+#         if pass1 != pass2:
+#             messages.error(request, "Passwords didn't matched!!")
+#             return redirect('home')
+        
+#         if not username.isalnum():
+#             messages.error(request, "Username must be Alpha-Numeric!!")
+#             return redirect('home')
+        
+#         myuser = User.objects.create_user(username, email, pass1)
+#         myuser.first_name = fname
+#         myuser.last_name = lname
+#         # myuser.is_active = False
+#         myuser.is_active = False
+#         myuser.save()
+#         messages.success(request, "Your Account has been created succesfully!! Please check your email to confirm your email address in order to activate your account.")
+
+        
+#         # Email Address Confirmation Email
+#         current_site = get_current_site(request)
+#         email_subject = "Confirm your Email"
+#         message2 = render_to_string('email_confirmation.html',{
+            
+#             'name': myuser.first_name,
+#             'domain': current_site.domain,
+#             'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+#             'token': generate_token.make_token(myuser)
+#         })
+#         email = EmailMessage(
+#         email_subject,
+#         message2,
+#         settings.EMAIL_HOST_USER,
+#         [myuser.email],
+#         )
+#         email.fail_silently = True
+#         email.send()
+        
+#         return redirect('signin')
+        
+        
+#     return render(request, "authentication/signup.html")
+
+
+# def activate(request,uidb64,token):
+#     try:
+#         uid = force_text(urlsafe_base64_decode(uidb64))
+#         myuser = User.objects.get(pk=uid)
+#     except (TypeError,ValueError,OverflowError,User.DoesNotExist):
+#         myuser = None
+
+#     if myuser is not None and generate_token.check_token(myuser,token):
+#         myuser.is_active = True
+#         # user.profile.signup_confirmation = True
+#         myuser.save()
+#         login(request,myuser)
+#         messages.success(request, "Your Account has been activated!!")
+#         return redirect('signin')
+#     else:
+#         return render(request,'activation_failed.html')
+
+
+# def signin(request):
+#     if request.method == 'POST':
+#         username = request.POST['username']
+#         pass1 = request.POST['pass1']
+        
+#         user = authenticate(username=username, password=pass1)
+        
+#         if user is not None:
+#             login(request, user)
+#             fname = user.first_name
+#             # messages.success(request, "Logged In Sucessfully!!")
+#             return render(request, "authentication/index.html",{"fname":fname})
+#         else:
+#             messages.error(request, "Bad Credentials!!")
+#             return redirect('home')
+    
+#     return render(request, "authentication/signin.html")
+
+
+# def signout(request):
+#     logout(request)
+#     messages.success(request, "Logged Out Successfully!!")
+#     return redirect('home')
